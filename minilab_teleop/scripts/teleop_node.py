90565a39495aca79176a8e9dab82b8a3f1287ae8
Salah <salahha1337@gmail.com>
2024-11-17 14:55:03 +0100

    Initial commit

---
+++ minilab_teleop/scripts/teleop_node.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import rospy
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+
+class TeleopNode:
+    def __init__(self):
+        rospy.init_node('minilab_teleop')
+        
+        # Paramètres
+        self.linear_scale = rospy.get_param('~linear_scale', 10.0)  # m/s
+        self.angular_scale = rospy.get_param('~angular_scale', 3.0)  # rad/s
+        
+        # Publishers et Subscribers
+        self.cmd_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.joy_sub = rospy.Subscriber('/joy', Joy, self.joy_callback)
+        
+        self.cmd_msg = Twist()
+        
+    def joy_callback(self, joy_msg):
+        # Axe vertical gauche pour vitesse linéaire
+        self.cmd_msg.linear.x = joy_msg.axes[5] * self.linear_scale
+        
+        # Axe horizontal gauche pour vitesse angulaire
+        self.cmd_msg.angular.z = joy_msg.axes[2] * self.angular_scale
+        
+        # Publier la commande
+        self.cmd_pub.publish(self.cmd_msg)
+
+if __name__ == '__main__':
+    try:
+        node = TeleopNode()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
